d51e80949b1214cb57535087a52cd4ec28f9ed52
Bruno Artur <batlopes@yahoo.com.br>
2021-04-03 23:11:28 -0300

    Filter by query parameters implemented and dependencies corrected.

--- api/viewsets.py
+++ api/viewsets.py
@@ -3,32 +3,68 @@ from .models import Degree, Job, Skill, DataOrigin, Address, Qualification
 from .serializers import DegreeSerializer, JobSerializer, SkillSerializer, DataOriginSerializer, \
     AddressSerializer, QualificationSerializer
 
-
 class AddressViewSet(ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+    filterset_fields = {
+        'zip_code': ['iexact', 'icontains'],
+        'country': ['iexact', 'icontains'],
+        'state': ['iexact', 'icontains'],
+        'city': ['iexact', 'icontains'],
+        'street': ['iexact', 'icontains'],
+        'street_number': ['iexact', 'icontains'],
+    }
 
 
 class DegreeViewSet(ModelViewSet):
     queryset = Degree.objects.all()
     serializer_class = DegreeSerializer
+    filterset_fields = {
+        'name': ['iexact', 'icontains'],
+        'description': ['icontains'],
+    }
 
 
 class QualificationViewSet(ModelViewSet):
     queryset = Qualification.objects.all()
     serializer_class = QualificationSerializer
+    filterset_fields = {
+        'name': ['iexact', 'icontains'],
+        'description': ['iexact', 'icontains'],
+        'degree_id__name': ['iexact', 'icontains'],
+        'degree_id__id': ['iexact'],
+    }
 
 
 class SkillViewSet(ModelViewSet):
     queryset = Skill.objects.all()
     serializer_class = SkillSerializer
+    filterset_fields = {
+        'name': ['iexact', 'icontains'],
+        'description': ['iexact', 'icontains'],
+        'origin_id__name': ['iexact', 'icontains'],
+        'origin_id__id': ['iexact'],
+    }
 
 
 class JobViewSet(ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+    filterset_fields = {
+        'title': ['iexact', 'icontains'],
+        'state': ['iexact', 'icontains'],
+        'description': ['iexact', 'icontains'],
+        'location': ['iexact', 'icontains'],
+        'origin_id__name': ['iexact', 'icontains'],
+        'origin_id__id': ['iexact', 'icontains'],
+        'skills__id': ['iexact']
+    }
 
 
 class DataOriginViewSet(ModelViewSet):
     queryset = DataOrigin.objects.all()
     serializer_class = DataOriginSerializer
+    filterset_fields = {
+        'name': ['iexact', 'icontains'],
+        'url': ['iexact', 'icontains'],
+    }
--- openjobs/settings.py
+++ openjobs/settings.py
@@ -130,7 +130,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 SIMPLE_JWT = {
     # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
