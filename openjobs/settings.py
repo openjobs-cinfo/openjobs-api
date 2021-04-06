29d8c90e16e0d2bbce7b4bb2f8a232313a817c0a
Bruno Artur <batlopes@yahoo.com.br>
2021-04-06 20:12:18 -0300

    Changes to AccessTokens Validity
    Access Tokens configured to have 1 day of validity.

--- openjobs/settings.py
+++ openjobs/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
     # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
